485700a8d111885acae2c90f70692ef3d65d1065
daychizu <60445491+daychizu@users.noreply.github.com>
2021-10-21 09:26:25 +0200

    Add files via upload

---
+++ js_calendar_from_ics-master/test.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $file = fopen("http://adelb.univ-lyon1.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=9311&projectId=1&calType=ical&firstDate=2021-09-03&lastDate=2022-06-25", "r");
+
+    $line = "";
+    
+      while(!feof($file)){
+
+        $line .= fgets($file);
+
+        }
+
+echo $line;
+ ?>

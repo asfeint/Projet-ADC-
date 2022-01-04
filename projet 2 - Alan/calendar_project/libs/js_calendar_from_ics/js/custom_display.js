55b5cb174ab11006b54fdfd5e387605b8df9cc98
Feint <60445491+daychizu@users.noreply.github.com>
2022-01-04 13:03:47 +0100

    Add files via upload

---
+++ projet 2 - Alan/calendar_project/libs/js_calendar_from_ics/js/custom_display.js
@@ -0,0 +1,136 @@
+// Edit your ics sources here
+ics_sources = [
+    {url:'../../src/php/get_ics_file_contents.php',event_properties: {color: 'Black'}}
+]
+
+
+////////////////////////////////////////////////////////////////////////////
+//
+// Here be dragons!
+//
+////////////////////////////////////////////////////////////////////////////
+
+function data_req (url, callback) {
+    req = new XMLHttpRequest()
+    req.addEventListener('load', callback)
+    req.open('GET', url)
+    req.send()
+}
+
+function add_recur_events() {
+    if (sources_to_load_cnt < 1) {
+        $('#calendar').fullCalendar('addEventSource', expand_recur_events)
+    } else {
+        setTimeout(add_recur_events, 30)
+    }
+}
+
+
+function load_ics(ics, cpt){
+    data_req(ics.url, function(){
+        $('#calendar').fullCalendar('addEventSource', fc_events(this.response, ics.event_properties))
+        sources_to_load_cnt -= 1;
+    })
+    // Meddling with the HTML to add everything related to our ics feeds dynamically
+    // hidden ics feeds
+    document.getElementById("ics-feeds").insertAdjacentHTML('beforeend', "<span hidden id='ics-url"+cpt+"'>"+ics.url+"</span>");
+
+    // calendar legend
+    document.getElementById("legend-feeds").insertAdjacentHTML('beforeend', "    <div class='calendar-feed'>" +
+        "<span class='fc-event-dot' style='background-color: "+ics.event_properties['color']+"'></span>" +
+        "<span> "+ics.title+" <button id='copyLink"+cpt+"'>" +
+        "<img src='./img/clipboard.svg' alt='copy to clipboard' title='copy to clipboard' width='15px' style='padding-top: 3px;'/></button></span></div>");
+
+    // copy button for ics feeds
+    document.querySelector("#copyLink"+cpt).addEventListener("click", function(){copy("ics-url"+cpt);});
+}
+
+
+$(document).ready(function() {
+
+    // display events
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay,listWeek,listMonth'
+        },
+        defaultView: 'agendaWeek',
+        firstDay: '1',
+        locale: 'fr',
+        lang: 'fr',
+
+        // customize the button names,
+        // otherwise they'd all just say "list"
+        views: {
+          listWeek: { buttonText: 'Liste Semaine' },
+          listMonth: { buttonText: 'Liste' }
+        },
+	navLinks: true,
+	editable: false,
+        eventLimit: true, // allow "more" link when too many events
+        eventRender: function(event, element, view) {
+            let desc = event.description
+            let newdesc = event.description.substring(0,desc.length-30);
+	  if(view.name == "listMonth" || view.name == "listWeek") {
+            element.find('.fc-list-item-title').append('<div style="margin-top:5px;"></div><span style="font-size: 0.9em">'+(event.description || 'no description')+'</span>'+((event.loc) ? ('<span style="margin-top:5px;display: block"><b>Venue: </b>'+event.loc+'</span>') : ' ')+'</div>');
+	  } else if(view.name == "agendaWeek" || view.name == "agendaDay") {
+            element.qtip({
+                content: {
+                  text: '<small>'+((event.start.format("d") != event.end.format("d")) ? (event.start.format("MMM Do")
+                        +(((event.end.subtract(1,"seconds")).format("d") == event.start.format("d")) ? ' ' : ' - '
+                        +(event.end.subtract(1,"seconds")).format("MMM Do"))) : (event.start.format("HH:mm")
+                        +' - '+event.end.format("HH:mm")))+'</small><br/>'+
+		         '<b>'+event.title+'</b>'+
+			 ((event.description) ? ('<br/>'+newdesc) : ' ')+
+			 ((event.loc) ? ('<br/><b>Venue: </b>'+event.loc) : ' ')
+                },
+                style: {
+                    classes: 'qtip-bootstrap qtip-rounded qtip-shadown qtip-light',
+                },
+                position: {
+                    my: 'center left',
+                    at: 'center right',
+                }
+            });
+
+        element.qtip.onclick = function(){window.open()}; 
+
+	  } else {
+            
+            element.qtip({
+                content: {
+                  text: '<small>'+((event.start.format("d") != event.end.format("d")) ? (event.start.format("MMM Do")
+                        +(((event.end.subtract(1,"seconds")).format("d") == event.start.format("d")) ? ' ' : ' - '
+                        +(event.end.subtract(1,"seconds")).format("MMM Do"))) : (event.start.format("HH:mm")
+                        +' - '+event.end.format("HH:mm")))+'</small><br/>'+
+		         '<b>'+event.title+'</b>'+
+			 ((event.description) ? ('<br/>'+newdesc) : ' ')+
+			 ((event.loc) ? ('<br/><b>Venue: </b>'+event.loc) : ' ')
+                },
+                style: {
+                    classes: 'qtip-bootstrap qtip-rounded qtip-shadown qtip-light',
+                },
+                position: {
+                    my: 'top left',
+                    at: 'bottom center',
+                }
+            });
+        
+        element.qtip.onclick = function(){window.open()}; 
+
+        }
+
+        }
+
+    })
+    sources_to_load_cnt = ics_sources.length
+    var cpt = 0;
+    for (ics of ics_sources) {
+        cpt+=1;
+        load_ics(ics, cpt)
+    }
+    add_recur_events()
+
+})
+
